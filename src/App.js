5bd99cefc0d4221d6769705d685b12fcea69369f
ZhenyaGoroh <zhenyagoroh@gmail.com>
2022-09-26 22:18:07 +0300

    without css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState} from "react";
+import "./App.css";
+import AddToDo from "./components/AddToDo/AddToDo";
+import Header from "./components/Header/Header";
+import ToDoList from "./components/ToDoList/ToDoList";
 
 function App() {
+  const [todo, setTodo] = useState(JSON.parse(localStorage.getItem("todo"))||[]);
+  
+  // 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <AddToDo todo={todo} setTodo={setTodo} />
+      <ToDoList todo={todo} setTodo={setTodo} />
     </div>
   );
 }
